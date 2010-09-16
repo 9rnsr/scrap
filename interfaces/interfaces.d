91cc887e751a882578d9e4f4af4fcb57dc6d23ce
k-hara <k.hara.pg+dev@gmail.com>
2010-09-16 18:31:16 +0900

    support const member function.

--- interfaces/interfaces.d
+++ interfaces/interfaces.d
@@ -26,12 +26,106 @@
 module interfaces;
 
 import std.traits, std.typecons, std.typetuple;
-import extypecons;
-
-import std.stdio;
 import std.functional;
 
-template MakeSignatureTbl(T, int Mode)
+//import std.stdio, std.string;
+
+
+/// 
+struct Interface(string def)
+{
+protected:	//privateだとなぜか駄目
+	mixin("interface I { " ~ def ~ "}");
+
+private:
+	alias MakeSignatureTbl!(I, 0).result allNames;
+	alias MakeSignatureTbl!(I, 1).result allFpSigs;
+	alias MakeSignatureTbl!(I, 2).result allDgSigs;
+	
+	void*				objptr;
+	Tuple!(allFpSigs)	funtbl;
+
+	template Sig2Idx(char stc, string Name, Args...)
+	{
+		template StorageClassCheck(string mangleof)
+		{
+			static assert(mangleof.length >= 2 && mangleof[0] == 'P');
+			
+			static if( stc == 'x' && mangleof[1] == 'x' && mangleof[2] == 'F' ){
+				enum StorageClassCheck = true;	//const
+			}else static if( stc == 'y' && mangleof[1] == 'y' && mangleof[2] == 'F' ){
+				enum StorageClassCheck = true;	//immutable
+			}else static if( stc == 'O' && mangleof[1] == 'O' && mangleof[2] == 'F'  ){
+				enum StorageClassCheck = true;	//shared
+			}else static if( stc == 'm' && mangleof[1] == 'F' ){
+				enum StorageClassCheck = true;	//mutable
+			}else{
+				enum StorageClassCheck = false;
+			}
+		}
+		template Impl(int N, string Name, Args...)
+		{
+			static if( N < allNames.length ){
+//				pragma(msg, "   find in Sig2Idx, [", N, "] ", stc, " ", allFpSigs[N], " ", allFpSigs[N].mangleof);
+				static if( allNames[N] == Name
+						&& is(ParameterTypeTuple!(allFpSigs[N]) == Args)
+						&& StorageClassCheck!(allFpSigs[N].mangleof) ){
+					enum result = N;
+				}else{
+					enum result = Impl!(N+1, Name, Args).result;
+				}
+			}else{
+				enum result = -1;
+			}
+		}
+		enum result = Impl!(0, Name, Args).result;
+	}
+
+public:
+	this(T)(T obj) if( isAllContains!(I, T)() ){
+		foreach( i, name; allNames ){
+			allDgSigs[i] dg = mixin("&obj." ~ name);
+			
+			static if( i == 0 ) objptr = dg.ptr;
+			funtbl.field[i] = dg.funcptr;
+//			writefln("[%s] : %08X <- %s", i, cast(void*)funtbl.field[i], allDgSigs[i].stringof);
+		}
+	}
+	
+	private enum dispatch =
+	q{
+		enum i = Sig2Idx!(stc, Name, Args).result;
+//		writefln("  %s Sig2Idx = %s", stc, i);
+		static assert(i >= 0,
+			"member '" ~ Name ~ "' not found in " ~ allNames.stringof);
+		return composeDg(cast(void*)objptr, funtbl.field[i])(args);
+	};
+	
+	auto opDispatch(string Name, Args...)(Args args)
+	{
+		enum stc = 'm';
+		mixin(dispatch);
+	}
+	auto opDispatch(string Name, Args...)(Args args) const
+	{
+		enum stc = 'x';
+//		pragma(msg, "opDispatch!(", Name, ", ", Args, ")");
+		mixin(dispatch);
+	}
+	auto opDispatch(string Name, Args...)(Args args) immutable
+	{
+		enum stc = 'y';
+		mixin(dispatch);
+	}
+	auto opDispatch(string Name, Args...)(Args args) shared
+	{
+		enum stc = 'O';
+		mixin(dispatch);
+	}
+}
+
+
+private template MakeSignatureTbl(T, int Mode)
 {
 	alias TypeTuple!(__traits(allMembers, T)) Names;
 	
@@ -79,89 +173,6 @@ template MakeSignatureTbl(T, int Mode)
 }
 
 
-
-/// 
-struct Interface(string def)
-{
-protected:	//privateだとなぜか駄目
-	mixin("interface I { " ~ def ~ "}");
-
-private:
-	alias MakeSignatureTbl!(I, 0).result allNames;
-	alias MakeSignatureTbl!(I, 1).result allFpSigs;
-	alias MakeSignatureTbl!(I, 2).result allDgSigs;
-	
-	void*				objptr;
-	Tuple!(allFpSigs)	funtbl;
-
-	template Sig2Idx(string Name, Args...)
-	{
-		template Impl(int N, string Name, Args...)
-		{
-			static if( N < allNames.length ){
-				static if( allNames[N] == Name
-						&& is(ParameterTypeTuple!(allFpSigs[N]) == Args) ){
-					enum result = N;
-				}else{
-					enum result = Impl!(N+1, Name, Args).result;
-				}
-			}else{
-				enum result = -1;
-			}
-		}
-		enum result = Impl!(0, Name, Args).result;
-	}
-
-public:
-	this(T)(T obj) if( isAllContains!(I, T)() ){
-		foreach( i, name; allNames ){
-			allDgSigs[i] dg = mixin("&obj." ~ name);
-			
-			static if( i == 0 ) objptr = dg.ptr;
-			funtbl.field[i] = dg.funcptr;
-			writefln("[%s] : %08X", i, funtbl.field[i]);
-		}
-	}
-	
-	auto opDispatch(string Name, Args...)(Args args)
-	{
-		enum i = Sig2Idx!(Name, Args).result;
-		static assert(i >= 0,
-			"member '" ~ Name ~ "' not found in " ~ allNames.stringof);
-		return composeDg(objptr, funtbl.field[i])(args);
-	}
-	auto opDispatch(string Name, Args...)(Args args) const
-	{
-		enum i = Sig2Idx!(Name, Args).result;
-		static assert(i >= 0,
-			"member '" ~ Name ~ "' not found in " ~ allNames.stringof);
-		return composeDg(objptr, funtbl.field[i])(args);
-	}
-}
-
-
-version(none)	//for 
-unittest
-{
-	static class A
-	{
-		int draw()				{ return 1; }
-		int draw() const		{ return 10; }
-	}
-	
-	alias Interface!q{
-		int draw();
-		int draw() const;
-	} Drawable;
-	
-	auto a = new A();
-	Drawable d = a;
-	const(Drawable) cd = a;
-	d.draw();
-	cd.draw();
-}
-
-
 private static bool isAllContains(I, T)()
 {
 	alias MakeSignatureTbl!(I, 0).result allNames;
@@ -172,7 +183,7 @@ private static bool isAllContains(I, T)()
 	
 	bool result = true;
 	foreach( i, name; allNames ){
-		pragma(msg, name, ": ", allFpSigs[i]);
+//		pragma(msg, name, ": ", allFpSigs[i]);
 		
 		bool res = false;
 		foreach( j, s; tgt_allNames ){
@@ -188,7 +199,6 @@ private static bool isAllContains(I, T)()
 	}
 	return result;
 }
-
 
 
 auto toDelegate(F)(auto ref F fp) if (isCallable!(F)) {
@@ -227,45 +237,156 @@ auto toDelegate(F)(auto ref F fp) if (isCallable!(F)) {
 
         DelegateFaker!(F) dummy;
         auto dummyDel = &(dummy.doIt);
-        df.funcPtr = cast(void*)dummyDel.funcptr;	//強制キャストを挟むことで、int delegate() const -> void* が可能になる
+        df.funcPtr = cast(void*) dummyDel.funcptr;
+        	//use cast expression for converting from R delegate(A...) const to void*
 
         return df.del;
     }
 }
 
 
-version(none)	//for test
+/// 
+auto composeDg(T...)(T args)
+{
+	static if( T.length==1 && is(Unqual!(T[0]) X == Tuple!(void*, U), U) ){
+		auto tup = args[0];
+		//alias typeof(tup.field[1]) U;
+		
+		auto ptr		= tup.field[0];
+		auto funcptr	= tup.field[1];
+		
+	}else static if( T.length==2 && is(Unqual!(T[0]) == void*) && isFunctionPointer!(Unqual!(T[1])) ){
+		auto ptr		= args[0];
+		auto funcptr	= args[1];
+		alias T[1] U;
+		
+	}else{
+		static assert(0, T.stringof);
+	}
+	
+	ReturnType!U delegate(ParameterTypeTuple!U) dg;
+	dg.ptr		= ptr;
+	dg.funcptr	= cast(typeof(dg.funcptr))funcptr;
+	return dg;
+}
+unittest
+{
+	int localfun(int n){ return n*10; }
+	
+	auto dg = &localfun;
+	assert(composeDg(dg.ptr, dg.funcptr)(5) == 50);
+}
+
+
+unittest
+{
+	static class A
+	{
+		int draw()				{ return 10; }
+		int draw() const		{ return 20; }
+		int draw() immutable	{ return 30; }
+		int draw() shared		{ return 40; }
+	}
+	
+	alias Interface!q{
+		int draw();
+		int draw() const;
+		int draw() immutable;
+		int draw() shared;
+	} Drawable;
+	
+	auto a = new A();
+	auto         d =           Drawable (a);
+	const       cd =     const(Drawable)(a);
+//	immutable   id = immutable(Drawable)(a);	// yet supported create immutable interface
+//	shared      sd =    shared(Drawable)(a);	// yet supported create shared interface
+	assert( d.draw() == 10);
+	assert(cd.draw() == 20);
+//	assert(id.draw() == 30);
+//	assert(sd.draw() == 40);
+}
+
+
+//version(none)	//for test
+unittest
+{
+	static class A
+	{
+		int draw()				{ return 10; }
+		int draw() const		{ return 20; }
+		int draw() immutable	{ return 30; }
+		int draw() shared		{ return 40; }
+	}
+	
+	alias Interface!q{
+		int draw();
+		int draw() const;
+		int draw() immutable;
+		int draw() shared;
+	} Drawable;
+	
+	Drawable d = new A();
+	assert( composeDg(d.objptr, d.funtbl.field[0])()  == 10);	// int draw()
+	assert( composeDg(d.objptr, d.funtbl.field[1])()  == 20);	// int draw() const
+//	assert( composeDg(d.objptr, d.funtbl.field[2])()  == 30);	// int draw() immutable <- invalid address
+	assert( composeDg(d.objptr, d.funtbl.field[3])()  == 40);	// int draw() shared
+}
+
+
+//version(none)	//for test
 unittest
 {
 	static class A
 	{
 		int draw()				{ return 1; }
 		int draw() const		{ return 10; }
+		int draw() immutable	{ return 20; }
+		int draw(int v)			{ return v*2; }
+		int draw(int v, int n)	{ return v*n; }
 	}
-	alias Interface!q{
-		int draw();
-		int draw() const;
-	} Drawable;
-	
-	Drawable d = new A();
-	assert( composeDg(d.objptr, d.funtbl.field[0])()  == 1);	// int draw()
-	assert( composeDg(d.objptr, d.funtbl.field[1])()  == 10);	// int draw() const
-}
-version(none)
-unittest
-{
-	class A
+	static class B
 	{
-		int draw()				{ return 1; }
-		int draw() const		{ return 10; }
+		int draw()				{ return 2; };
 	}
-	alias Interface!q{
-		int draw();
-		int draw() const;
-	} Drawable;
-	
-	Drawable d = new A();
-	assert( composeDg(d.objptr, d.funtbl.field[0])()  == 1);	// int draw()
-	assert( composeDg(d.objptr, d.funtbl.field[1])()  == 10);	// int draw() const
-}
+	static class X
+	{
+		void undef(){}
+	}
+	static class Y
+	{
+		void draw(double f){}
+	}
 
+	{
+		alias Interface!q{
+			int draw();
+		} Drawable;
+		
+		Drawable d = new A();
+		assert(d.draw() == 1);
+		
+		d = Drawable(new B());
+		assert(d.draw() == 2);
+		
+		static assert(!__traits(compiles, d = Drawable(new X())));
+	}
+	{
+		alias Interface!q{
+			int draw(int v);
+		} Drawable;
+		
+		Drawable d = new A();
+		static assert(!__traits(compiles, d.draw()));
+		assert(d.draw(8) == 16);
+	}
+	{
+		alias Interface!q{
+			int draw(int v, int n);
+		} Drawable;
+		
+		Drawable d = new A();
+		assert(d.draw(8, 8) == 64);
+		
+		static assert(!__traits(compiles, d = Drawable(new Y())));
+	}
+}
