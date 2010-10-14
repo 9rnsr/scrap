92b52c522b2322059e188b7c4ebcb6de0ed2de17
k-hara <k.hara.pg+dev@gmail.com>
2010-10-14 19:13:51 +0900

    remove staticMap workaround (via phobos r2017)

--- interfaces/meta_expand.d
+++ interfaces/meta_expand.d
@@ -5,208 +5,204 @@ import std.string;
 import std.stdio;
 
 
-bool isoctdigit(dchar c){ return '0'<=c && c<='7'; }
-bool ishexdigit(dchar c){ return ('0'<=c && c<='9') || ('A'<=c && c<='F') || ('a'<=c && c<='f'); }
-
-struct ParseResult
+private
 {
-	string result;
-	string remain;
-	
-	bool opCast(T)() if(is(T==bool)) { return result.length > 0; }
-	
-	this(string res, string rem){
-		result = res;
-		remain = rem;
-	}
-	this(string rem){
-		result = null;
-		remain = rem;
-	}
-}
+	bool isoctdigit(dchar c){ return '0'<=c && c<='7'; }
+	bool ishexdigit(dchar c){ return ('0'<=c && c<='9') || ('A'<=c && c<='F') || ('a'<=c && c<='f'); }
 
-/+
-template parseCharLiteral(string code)
-{
-	template parse(string code)
+	struct ParseResult
 	{
-		static if( code.length == 0 ){
-			enum result = 0;
-		}else static if( code[0] == 'x' &&
-			code.length>=2 && ishexdigit(code[1]) &&
-			code.length>=3 && ishexdigit(code[2]) ){
-			enum result = 3;
-		}else static if( isoctdigit(code[0]) ){
-			static if( isoctdigit(code[1]) ){
-				static if( isoctdigit(code[2]) ){
-					enum result = 3;
-				}else{
-					enum result = 2;
-				}
-			}else{
-				enum result = 1;
-			}
-		}else static if( code[0] == 'u' &&
-			code.length>=2 && ishexdigit(code[1]) &&
-			code.length>=3 && ishexdigit(code[2]) &&
-			code.length>=4 && ishexdigit(code[3]) &&
-			code.length>=5 && ishexdigit(code[4]) ){
-				enum result = 5;
-		}else static if( code[0] == 'u' &&
-			code.length>=2 && ishexdigit(code[1]) &&
-			code.length>=3 && ishexdigit(code[2]) &&
-			code.length>=4 && ishexdigit(code[3]) &&
-			code.length>=5 && ishexdigit(code[4]) &&
-			code.length>=6 && ishexdigit(code[5]) &&
-			code.length>=7 && ishexdigit(code[6]) &&
-			code.length>=8 && ishexdigit(code[7]) &&
-			code.length>=9 && ishexdigit(code[8]) ){
-				enum result = 9;
-		}else static if( code[0] == '&' ){
-			static assert(0, "not implemented"); 
-		}else{
-			enum result = 1;
+		string result;
+		string remain;
+		
+		bool opCast(T)() if (is(T==bool))
+		{
+			return result.length > 0;
+		}
+		
+		this(string res, string rem)
+		{
+			result = res;
+			remain = rem;
+		}
+		this(string rem)
+		{
+			result = null;
+			remain = rem;
 		}
 	}
-	static if( code[0] == '\\' ){
-		enum len = 1 + parse!(code[1..$]).result;
-		enum result = code[0..len];
-		enum remain = code[len..$];
-	}else{
-		enum result = null;
-		enum remain = code;
-	}
-}+/
 
-ParseResult parseStr(string code)
-{
-	auto remain = chompPrefix(code, `"`);
-	if( remain.length < code.length ){
-		size_t i = code.length - remain.length;
-		auto result = code[0 .. i];
-		for( ; i<code.length; ++i ){
-			auto pVar = parseVar(code[i..$]);
-			if( pVar ){
+	ParseResult parseStr(string code)
+	{
+		auto remain = chompPrefix(code, `"`);
+		if (remain.length < code.length)
+		{
+			size_t i = code.length - remain.length;
+			auto result = code[0 .. i];
+			for (; i<code.length; ++i)
+			{
+				auto pVar = parseVar(code[i..$]);
+				if (pVar)
+				{
+					result ~= "`~"
+							~ pVar.result[2..$-1]
+							~ "~`";
+					i += pVar.result.length;
+				}
+				
+				if (code[i] == '\\')
+				{
+					result ~= code[i];
+					++i;
+				}
+				else if (code[i] == '\"')
+				{
+					result ~= code[i];
+					return ParseResult(result, code[i+1..$]);
+				}
+				result ~= code[i];
+			}
+		}
+		return ParseResult(code);
+	}
+
+	ParseResult parseAltStr(string code)
+	{
+		auto remain = chompPrefix(code, "`");
+		if (remain.length < code.length)
+		{
+			foreach (i; 0..remain.length)
+			{
+				if (remain[i] == '`')
+				{
+					return ParseResult(code[0..1+i+1], remain[i+1..$]);
+				}
+			}
+		}
+		return ParseResult(code);
+	}
+
+	ParseResult parseRawStr(string code)
+	{
+		auto remain = chompPrefix(code, `r"`);
+		if (remain.length < code.length)
+		{
+			foreach (i; 0..remain.length)
+			{
+				if (remain[i] == '\"')
+				{
+					return ParseResult(code[0..2+i+1], remain[i+1..$]);
+				}
+			}
+		}
+		return ParseResult(code);
+	}
+
+	ParseResult parseVar(string code)
+	{
+		auto remain = chompPrefix(code, `${`);
+		if (remain.length < code.length)
+		{
+			foreach (i; 0..remain.length)
+			{
+				if (remain[i] == '}')
+				{
+					return ParseResult(code[0..2+i+1], remain[i+1..$]);
+				}
+			}
+		}
+		return ParseResult(code);
+	}
+
+	string expandCode(string code)
+	{
+		auto remain = code;
+		auto result = "";
+		while (remain.length)
+		{
+			auto pStr    = parseStr(remain);
+			auto pAltStr = parseAltStr(remain);
+			auto pRawStr = parseRawStr(remain);
+			auto pVar    = parseVar(remain);
+			
+			if (pStr)
+			{
+				result ~= pStr.result;
+				remain  = pStr.remain;
+			}
+			else if (pAltStr)
+			{
+				result ~= "`~\"`\"~`"
+						~ pAltStr.result[1..$-1]
+						~ "`~\"`\"~`";
+				remain  = pAltStr.remain;
+			}
+			else if( pRawStr )
+			{
+				result ~= pRawStr.result;
+				remain  = pRawStr.remain;
+			}
+			else if (pVar)
+			{
 				result ~= "`~"
 						~ pVar.result[2..$-1]
 						~ "~`";
-				i += pVar.result.length;
+				remain  = pVar.remain;
 			}
-			
-			if( code[i] == '\\' ){
-				result ~= code[i];
-				++i;
-			}else if( code[i] == '\"' ){
-				result ~= code[i];
-				return ParseResult(result, code[i+1..$]);
-			}
-			result ~= code[i];
-		}
-	}
-	return ParseResult(code);
-}
-
-ParseResult parseAltStr(string code)
-{
-	auto remain = chompPrefix(code, "`");
-	if( remain.length < code.length ){
-		for( size_t i=0; i<remain.length; ++i ){
-			if( remain[i] == '`' ){
-				return ParseResult(code[0..1+i+1], remain[i+1..$]);
+			else
+			{
+				result ~= remain[0];
+				remain  = remain[1..$];
 			}
 		}
+		return result;
 	}
-	return ParseResult(code);
 }
-
-ParseResult parseRawStr(string code)
-{
-	auto remain = chompPrefix(code, `r"`);
-	if( remain.length < code.length ){
-		for( size_t i=0; i<remain.length; ++i ){
-			if( remain[i] == '\"' ){
-				return ParseResult(code[0..2+i+1], remain[i+1..$]);
-			}
-		}
+/**
+	Expand expression in code string
+	----
+	enum string op = "+";
+	static assert(expand!q{ 1 ${op} 2 } == q{ 1 + 2 });
+	----
+	
+	Using both mixin expression, it is easy making parameterized code-blocks.
+	----
+	template DeclFunc(string name)
+	{
+		mixin(expand!q{
+			int ${name}(int a){ return a; }
+		});
 	}
-	return ParseResult(code);
-}
-
-ParseResult parseVar(string code)
-{
-	auto remain = chompPrefix(code, `${`);
-	if( remain.length < code.length ){
-		for( size_t i=0; i<remain.length; ++i ){
-			if( remain[i] == '}' ){
-				return ParseResult(code[0..2+i+1], remain[i+1..$]);
-			}
-		}
-	}
-	return ParseResult(code);
-}
-
-string expandCode(string code)
-{
-	auto remain = code;
-	auto result = "";
-	while( remain.length ){
-		auto pStr		= parseStr(remain);
-		auto pAltStr	= parseAltStr(remain);
-		auto pRawStr	= parseRawStr(remain);
-		auto pVar		= parseVar(remain);
-		
-		if( pStr ){
-			result ~= pStr.result;
-			remain  = pStr.remain;
-		}else if( pAltStr ){
-			result ~= "`~\"`\"~`"
-					~ pAltStr.result[1..$-1]
-					~ "`~\"`\"~`";
-			remain  = pAltStr.remain;
-		//	assert(0, result ~ "/" ~ remain);
-		}else if( pRawStr ){
-			result ~= pRawStr.result;
-			remain  = pRawStr.remain;
-		}else if( pVar ){
-			result ~= "`~"
-					~ pVar.result[2..$-1]
-					~ "~`";
-			remain  = pVar.remain;
-		}else{
-			result ~= remain[0];
-			remain  = remain[1..$];
-		}
-	}
-	return result;
-}
-
+	----
+	DeclFunc template generates specified name function.
+ */
 template expand(string code)
 {
 	enum expand = "`" ~ expandCode(code) ~ "`";
 }
 
 
-version(none) void main(){}
-
-
-template ExpandTest(string op, string from)
+version(unittest)
 {
-	enum ExpandTest = mixin(expand!from);
+	template ExpandTest(string op, string from)
+	{
+		enum ExpandTest = mixin(expand!from);
+	}
+	static assert(ExpandTest!("+", q{a ${op} b})     == q{a + b});
+	static assert(ExpandTest!("+", q{`raw string`})  == q{`raw string`});
+	static assert(ExpandTest!("+", q{"a ${op} b"})   == q{"a + b"});
+	static assert(ExpandTest!("+", q{r"${op}"})      == q{r"${op}"});
+	static assert(ExpandTest!("+", q{`${op}`})       == q{`${op}`});
+	static assert(ExpandTest!("+", q{"\a"})          == q{"\a"});
+	static assert(ExpandTest!("+", q{"\xA1"})        == q{"\xA1"});
+	static assert(ExpandTest!("+", q{"\0"})          == q{"\0"});
+	static assert(ExpandTest!("+", q{"\01"})         == q{"\01"});
+	static assert(ExpandTest!("+", q{"\012"})        == q{"\012"});
+	static assert(ExpandTest!("+", q{"\u0FFF"})      == q{"\u0FFF"});
+	static assert(ExpandTest!("+", q{"\U00000FFF"})  == q{"\U00000FFF"});
+
+	static assert(ExpandTest!("+", q{"\""})          == q{"\""});
+
+	static assert(ExpandTest!("+", q{${op} ${op}})   == q{+ +});
+	static assert(ExpandTest!("+", q{"${op} ${op}"}) == q{"+ +"});
 }
-static assert(ExpandTest!("+", q{a ${op} b})	== q{a + b});
-static assert(ExpandTest!("+", q{`raw string`}) == q{`raw string`});
-static assert(ExpandTest!("+", q{"a ${op} b"})	== q{"a + b"});
-static assert(ExpandTest!("+", q{r"${op}"})		== q{r"${op}"});
-static assert(ExpandTest!("+", q{`${op}`})		== q{`${op}`});
-static assert(ExpandTest!("+", q{"\a"})			== q{"\a"});
-static assert(ExpandTest!("+", q{"\xA1"})		== q{"\xA1"});
-static assert(ExpandTest!("+", q{"\0"})			== q{"\0"});
-static assert(ExpandTest!("+", q{"\01"})		== q{"\01"});
-static assert(ExpandTest!("+", q{"\012"})		== q{"\012"});
-static assert(ExpandTest!("+", q{"\u0FFF"})		== q{"\u0FFF"});
-static assert(ExpandTest!("+", q{"\U00000FFF"})	== q{"\U00000FFF"});
-
-static assert(ExpandTest!("+", q{"\""})			== q{"\""});
-
-static assert(ExpandTest!("+", q{${op} ${op}})		== q{+ +});
-static assert(ExpandTest!("+", q{"${op} ${op}"})	== q{"+ +"});
--- interfaces/meta_forward.d
+++ interfaces/meta_forward.d
@@ -1,70 +1,61 @@
 module meta_forward;
 
-import std.traits;
-import std.typetuple;
-
-/// 
+/**
+ */
 template Forward(F, string name, string code)
 {
 private:
-	//import std.typetuple;
-	private template staticMap(alias F, T...)	//workaround
-	{
-		static if (T.length == 0)
-		{
-			alias TypeTuple!() staticMap;
-		}
-		else
-		{
-			alias TypeTuple!(F!(T[0]), staticMap!(F, T[1 .. $])) staticMap;
-		}
-	}
-
-	import std.traits;
+	import std.traits, std.typetuple;
 	import std.conv;
 	import meta_expand;
 	
 	enum paramName = "a";
 	template ForwardImpl(F)
 	{
-		template PrmSTC2String(uint stc)
+		template PrmSTC2Str(uint stc)
 		{
-			static if( stc == ParameterStorageClass.NONE )
+			static if (stc == ParameterStorageClass.NONE)
 			{
-				enum PrmSTC2String = "";
+				enum PrmSTC2Str = "";
 			}
-			else static if( stc & ParameterStorageClass.SCOPE )
+			else static if (stc & ParameterStorageClass.SCOPE)
 			{
-				enum PrmSTC2String = "scope " ~ PrmSTC2String!(stc & ~ParameterStorageClass.SCOPE);
+				enum PrmSTC2Str = "scope "
+					~ PrmSTC2Str!(stc & ~ParameterStorageClass.SCOPE);
 			}
-			else static if( stc & ParameterStorageClass.OUT )
+			else static if (stc & ParameterStorageClass.OUT)
 			{
-				enum PrmSTC2String = "out " ~ PrmSTC2String!(stc & ~ParameterStorageClass.OUT);
+				enum PrmSTC2Str = "out "
+					~ PrmSTC2Str!(stc & ~ParameterStorageClass.OUT);
 			}
-			else static if( stc & ParameterStorageClass.REF )
+			else static if (stc & ParameterStorageClass.REF)
 			{
-				enum PrmSTC2String = "ref " ~ PrmSTC2String!(stc & ~ParameterStorageClass.REF);
+				enum PrmSTC2Str = "ref "
+					~ PrmSTC2Str!(stc & ~ParameterStorageClass.REF);
 			}
-			else static if( stc & ParameterStorageClass.LAZY )
+			else static if (stc & ParameterStorageClass.LAZY)
 			{
-				enum PrmSTC2String = "lazy " ~ PrmSTC2String!(stc & ~ParameterStorageClass.LAZY);
+				enum PrmSTC2Str = "lazy "
+					~ PrmSTC2Str!(stc & ~ParameterStorageClass.LAZY);
 			}
 		}
-		static string ParameterSTCs(int mode)
+		static string PrmSTCs(int mode)
 		{
-//			pragma(msg, ParameterStorageClassTuple!F);
-			alias staticMap!(PrmSTC2String, ParameterStorageClassTuple!F) PrmSTCs;
-//			pragma(msg, ParameterStorageClassTuple!F, " -> [", PrmSTCs, "]");
+			alias staticMap!(PrmSTC2Str, ParameterStorageClassTuple!F) pstcs;
 			
 			string result;
-			foreach( i, stc ; PrmSTCs )
+			foreach (i, stc ; pstcs)
 			{
-				if( i > 0 ) result ~= ", ";
-				if( mode == 0 )			// Parameter defines
+				if (i > 0)
+					result ~= ", ";
+				if (mode == 0)      // Parameter defines
 				{
-					result ~= PrmSTCs[i] ~ mixin(expand!q{ParameterTypeTuple!F[${to!string(i)}] }) ~ paramName ~ to!string(i);
+					result ~= pstcs[i]
+						~ mixin(expand!q{
+							ParameterTypeTuple!F[${to!string(i)}]
+						}) ~ paramName ~ to!string(i);
 				}
-				else if( mode == 1 )	// Parameter names
+				else if (mode == 1) // Parameter names
 				{
 					result ~= paramName ~ to!string(i);
 				}
@@ -72,18 +63,18 @@ private:
 			return result;
 		}
 		
-		static string FunctionSTCs()
+		static string FunSTCs()
 		{
 			string result;
-			static if( is(F == shared) )
+			static if (is(F == shared))
 			{
 				result ~= "shared ";
 			}
-			static if( is(F == const) )
+			static if (is(F == const))
 			{
 				result ~= "const ";
 			}
-			static if( is(F == immutable) )
+			static if (is(F == immutable))
 			{
 				result ~= "immutable ";
 			}
@@ -91,18 +82,13 @@ private:
 		}
 	}
 	alias ForwardImpl!F Impl;
-	
-//	pragma(msg, Impl.ParameterSTCs(0));
-//	pragma(msg, expand!q{ ReturnType!F ${name}(${Impl.ParameterSTCs(0)}){ mixin(code); } });
-//	pragma(msg, mixin(expand!q{ ReturnType!F ${name}(${Impl.ParameterSTCs(0)}){ mixin(code); } }));
 
 public:
-//	pragma(msg, "Forward.code : ", code);
 	mixin(
 		mixin(expand!
 		q{
-			ReturnType!F ${name}(${Impl.ParameterSTCs(0)}) ${Impl.FunctionSTCs()} {
-				alias TypeTuple!(${Impl.ParameterSTCs(1)}) args;
+			ReturnType!F ${name}(${Impl.PrmSTCs(0)}) ${Impl.FunSTCs()} {
+				alias TypeTuple!(${Impl.PrmSTCs(1)}) args;
 				mixin(code);
 			}
 		})
@@ -123,33 +109,31 @@ unittest
 	
 	auto c = new C();
 	double v = 1.0;
-	assert(c.f(1, v) == 20);	assert(v == 2.0);
-	assert(c.g(1, v) == 30);	assert(v == 6.0);
+	assert(c.f(1, v) == 20);  assert(v == 2.0);
+	assert(c.g(1, v) == 30);  assert(v == 6.0);
 }
-
-
 unittest
 {
 	static class C
 	{
-		int f()				{ return 10; }
-		int g() const		{ return 20; }
-		int h() shared		{ return 30; }
+		int f()             { return 10; }
+		int g() const       { return 20; }
+		int h() shared      { return 30; }
 		int i() shared const{ return 40; }
-		int j() immutable	{ return 50; }
+		int j() immutable   { return 50; }
 		
 		// for overload set
-		mixin Forward!(typeof(f), "a1", q{ return f(); });		alias a1 a;
-		mixin Forward!(typeof(g), "a2", q{ return g(); });		alias a2 a;
-		mixin Forward!(typeof(h), "a3", q{ return h(); });		alias a3 a;
-		mixin Forward!(typeof(i), "a4", q{ return i(); });		alias a4 a;
-		mixin Forward!(typeof(j), "a5", q{ return j(); });		alias a5 a;
+		mixin Forward!(typeof(f), "a1", q{ return f(); });  alias a1 a;
+		mixin Forward!(typeof(g), "a2", q{ return g(); });  alias a2 a;
+		mixin Forward!(typeof(h), "a3", q{ return h(); });  alias a3 a;
+		mixin Forward!(typeof(i), "a4", q{ return i(); });  alias a4 a;
+		mixin Forward!(typeof(j), "a5", q{ return j(); });  alias a5 a;
 	}
 	auto           c = new C();
 	const         cc = new C();
-	shared        sc = cast(shared)new C();
-	shared const scc = cast(shared const)new C();
-	immutable     ic = cast(immutable)new C();
+	shared        sc = new shared(C)();
+	shared const scc = new shared(const(C))();
+	immutable     ic = new immutable(C)();
 	assert(  c.a() == 10);
 	assert( cc.a() == 20);
 	assert( sc.a() == 30);
