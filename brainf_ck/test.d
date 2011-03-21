40214bebe9d6d6b32b0f280f2d56b0cd6245be99
k-hara <k.hara.pg+dev@gmail.com>
2011-03-22 01:39:21 +0900

    Brainf*ck interpreter.

---
+++ brainf_ck/test.d
@@ -0,0 +1,78 @@
+import std.stdio;
+
+void main()
+{
+	Brainf.ck(
+		`+++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+++.>-.`
+		`------------.<++++++++.--------.+++.------.--------.>+.`
+	);
+	
+}
+
+
+class Brainf
+{
+static:
+	size_t ptr;
+	ubyte[100] buf;
+	
+	
+	void ck(string code)
+	{
+		eval(code);
+	}
+	void eval(string code)
+	{
+		void error()
+		{
+			throw new Exception("syntax error");
+		}
+		
+		size_t pc = 0;
+		while (pc < code.length)
+		{
+			switch (code[pc])
+			{
+				case '>':
+					++ptr;
+					break;
+				case '<':
+					--ptr;
+					break;
+				case '+':
+					++buf[ptr];
+					break;
+				case '-':
+					--buf[ptr];
+					break;
+				case '.':
+					writef("%s", cast(char)buf[ptr]);
+					break;
+				case ',':
+					ubyte n;
+					readf("%c", &n);
+					buf[ptr] = n;
+					break;
+				case '[':
+					if (buf[ptr] == 0)
+					{
+						while(code || code[pc] != ']')
+							++pc;
+						continue;
+					}
+					break;
+				case ']':
+					if (buf[ptr] != 0)
+					{
+						while(pc > 0 && code[--pc] != '['){}
+						if (code[pc] != '[') error();
+						continue;
+					}
+					break;
+				default:
+					error();
+			}
+			++pc;
+		}
+	}
+}
