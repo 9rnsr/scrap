7e93ac0a996da1ecdb1a495c9e2a7f80045d82cd
k-hara <k.hara.pg+dev@gmail.com>
2011-03-24 06:52:40 +0900

    fix indent and entab.

--- scoped/scoped.d
+++ scoped/scoped.d
@@ -5,31 +5,31 @@ Scoped!T scoped(T, A...)(A args)
 	// return value through hidden pointer.
 	static assert((Scoped!T).sizeof > 8, "too small object");
 	
-  //debug(0)
-  //{
-	ubyte* hidden;	// for assertion check
-	asm{ mov hidden, EAX; }
-  //}
-	
-  version(none)
-  {
-	auto s = Scoped!T();	// allocated on hidden[0 .. (Scoped!T).sizeof]
-	assert(cast(void*)&s == cast(void*)hidden);
-	emplace!T(cast(void[])s.__payload, args);
-	return s;	// destructor defined object cannnot RVO through hidden pointer
-  }
-  else
-  {
-	auto s = cast(Scoped!T*)hidden;
-	emplace!T(cast(void[])s.__payload, args);
-	asm{
-		pop EDI;
-		pop ESI;
-		pop EBX;
-		leave;
-		ret;
+	//debug(0)
+	//{
+		ubyte* hidden;	// for assertion check
+		asm{ mov hidden, EAX; }
+	//}
+
+	version(none)
+	{
+		auto s = Scoped!T();	// allocated on hidden[0 .. (Scoped!T).sizeof]
+		assert(cast(void*)&s == cast(void*)hidden);
+		emplace!T(cast(void[])s.__payload, args);
+		return s;	// destructor defined object cannnot RVO through hidden pointer
 	}
-  }
+	else
+	{
+		auto s = cast(Scoped!T*)hidden;
+		emplace!T(cast(void[])s.__payload, args);
+		asm{
+			pop EDI;
+			pop ESI;
+			pop EBX;
+			leave;
+			ret;
+		}
+	}
 }
 
 private extern (C) static void _d_monitordelete(Object h, bool det);
@@ -40,33 +40,36 @@ struct Scoped(T) if (is(T == class))
 	ubyte[__traits(classInstanceSize, T)] __payload;
 	@property T __object(){ return cast(T)__payload.ptr; }
 
-    @disable this(this)
-    {
-        writeln("Illegal call to Scoped this(this)");
-        assert(false);
-    }
+	@disable this(this)
+	{
+		writeln("Illegal call to Scoped this(this)");
+		assert(false);
+	}
 
-    ~this()
-    {
+	~this()
+	{
 		static void destroy(T)(T obj)
 		{
-		  static if (is(typeof(obj.__dtor())))
-		    obj.__dtor();
-		  static if (!is(T == Object) && is(T Base == super))
-		  {
-		    Base b = obj;
-		    destroy(b);
-		  }
+			static if (is(typeof(obj.__dtor())))
+			{
+				obj.__dtor();
+			}
+			static if (!is(T == Object) && is(T Base == super))
+			{
+				Base b = obj;
+				destroy(b);
+			}
 		}
 
-        destroy(__object);
-        if ((cast(void**)__payload.ptr)[1]) // if monitor is not null
-        {
-            _d_monitordelete(__object, true);
-        }
-    }
+		destroy(__object);
+		if ((cast(void**)__payload.ptr)[1])	// if monitor is not null
+		{
+			_d_monitordelete(__object, true);
+		}
+	}
 
 	alias __object this;
+	//mixin valueProxy!__object;	// blocking conversion Scoped!T to T, may need?
 }
 
 
@@ -97,5 +100,5 @@ void main()
 		a1.a = a1;
 		assert(a1.check());
 	}
-	assert(A.cnt == 0);
+	assert(A.cnt == 0);	// destructors called on scope exit
 }
